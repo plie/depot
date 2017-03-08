84881976f73e86b9be013bcc39a582d1191f04df
Jenee Geer <jeneegeer@gmail.com>
2017-03-08 11:40:26 -0600

    first set up Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223020448) do
+ActiveRecord::Schema.define(version: 20170308173704) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
